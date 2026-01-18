99fa823e750d8277586686a1c97a40c3442a59de
Marco Ghiglia <mark.ghiglia@gmail.com>
2026-01-18 23:36:03 +0100

    Create script.js per animazione scroll

---
+++ script.js
@@ -0,0 +1,16 @@
+function revealOnScroll() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    for (let el of reveals) {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+        const revealPoint = 120;
+
+        if (elementTop < windowHeight - revealPoint) {
+            el.classList.add('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
